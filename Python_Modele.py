8419a99c8a5e94abc70a5e1d264b92f115e01176
Bernard Mayer <25210782+BernardMayer@users.noreply.github.com>
2020-02-03 15:48:06 +0100

    Modele de script Python (MaJ)

--- Python_Modele.py
+++ Python_Modele.py
@@ -3,7 +3,7 @@
 
 
 """
-gps2db <db> <fichier.gpx>
+gps2db <db fichier.sqlite> <fichier.gpx>
 
 connecter à la db
 ouvrir le fichier GPX
@@ -24,3 +24,17 @@ rendre compte
 # print >> sys.stderr, "erreur"   -->   print("Erreur", file=sys.stderr)
 # print "une ligne ",             -->   print("une ligne", end="")
 from __future__ import print_function
+import os, sys
+
+##  Controler les 2 parametres de la ligne de commande
+if (len(sys.argv) != 3) :
+    print("usage : " + sys.argv[0] + " dirAdh <fichier.sqlite> <fichier.gpx>", file=sys.stderr)
+    sys.exit()
+else :
+    if (os.path.isfile(sys.argv[1]) and os.path.isfile(sys.argv[2])) :
+        print("Ok pour [" & sys.argv[1] & "] et [" & sys.argv[2] & "]")
+        dbFilename = sys.argv[1]
+        gpxFilename = sys.argv[2]
+    else :
+        print("ERR : Les fichiers indiques en parametres sont innacessibles !", file=sys.stderr)
+        
